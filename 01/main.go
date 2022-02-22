776b150881777c33d14dd713c303286bcb069c15
mesqute <mesqute@github.com>
2022-02-22 14:46:08 +0300

    Initial commit

---
+++ 01/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// Human это родительская структура, обладающая свойствами и методами
+type Human struct {
+	Age,
+	Weight int
+}
+
+// Action это структура паразит, использующая методы и свойства структуры Human
+type Action struct {
+	Human
+}
+
+// Eat повышает вес Human
+func (h *Human) Eat() {
+	h.Weight++
+}
+
+// Wait повышает возраст Human
+func (h *Human) Wait() {
+	h.Age++
+}
+
+func main() {
+	// инициализация структуры паразита
+	action := Action{Human{}}
+	// вывод данных, не принадлежащих структуре
+	fmt.Println("Возраст:", action.Age, "Вес:", action.Weight)
+	// выполнение действий
+	action.Eat()
+	action.Wait()
+	// повторный вывод для демонстрации изменений
+	fmt.Println("Возраст:", action.Age, "Вес:", action.Weight)
+
+}
